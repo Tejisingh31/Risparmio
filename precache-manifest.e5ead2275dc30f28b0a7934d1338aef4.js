675cbf987259c9f107197a3a24bb64d078d7bed2
tejisingh31 <httsingh1@gmail.com>
2024-11-07 22:32:49 +0100

    Updates

---
+++ precache-manifest.e5ead2275dc30f28b0a7934d1338aef4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8b82557bdb6ec3d1b425260c27928ab",
+    "url": "/Tejisingh31/Risparmio/index.html"
+  },
+  {
+    "revision": "6768a0098db8bde2c350",
+    "url": "/Tejisingh31/Risparmio/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "aea54c2cc0e9daa36ce6",
+    "url": "/Tejisingh31/Risparmio/static/js/2.bbc244fd.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/Tejisingh31/Risparmio/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "6768a0098db8bde2c350",
+    "url": "/Tejisingh31/Risparmio/static/js/main.6ebf5451.chunk.js"
+  },
+  {
+    "revision": "1b5d463e6efa17761fdb",
+    "url": "/Tejisingh31/Risparmio/static/js/runtime~main.3abc164b.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Tejisingh31/Risparmio/static/media/logo.06e73328.svg"
+  }
+]);
